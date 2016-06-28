fd6d5611528d4eb66dd6ba0c5d1d8a3503162a98
Bernhard Brodowsky <brodowsb@ethz.ch>
2016-06-28 21:27:54 +0200

    Extended dict

--- lib/dict.rb
+++ lib/dict.rb
@@ -8,6 +8,10 @@ class Dict
     @words ||= read_words
   end
 
+  def words_by_letter
+    @words_by_letter ||= words.group_by { |w| w[0].downcase }
+  end
+
   def read_words
     lines = DICT_PATHS.collect_concat { |p| File.readlines(p) }
     words = Set[]
@@ -15,13 +19,18 @@ class Dict
       prefix_exists = 3.upto(l.length).any? { |i| words.include? l[0...i] }
       words.add(l.chomp) unless prefix_exists
     end
-    words.to_a.sort.group_by { |w| w[0].downcase }
+    words.to_a.sort
   end
 
   private :read_words
 
   def words_for_regexp(start_letter, regexp)
-    words[start_letter].select { |w| w =~ regexp }
+    words_by_letter[start_letter].select { |w| w =~ regexp }
+  end
+
+  # Returns all words that containt the given part somewhere inside.
+  def words_with_part(part)
+    words.select { |w| w.include?(part) }
   end
 
 end
