d3aa95a095586bfca8dd60d43c9fe62086f90cea
Bernhard Brodowsky <brodowsb@ethz.ch>
2017-11-09 00:22:41 +0100

    Added letter

--- bin/corner_commutators.rb
+++ bin/corner_commutators.rb
@@ -14,9 +14,9 @@ include ConsoleHelpers
 # TODO Do this in the UI.
 
 results_model = ResultsModel.new(:corner_commutators)
-known_letters = ['a', 'b', 'd', 'e', 'j', 'g']
-new_letter = ['i']
-generator = SomeLettersCornerCommutators.new(results_model, new_letter)
+known_letters = ['a', 'b', 'd', 'e', 'j', 'g', 'i']
+new_letter = ['m']
+generator = SomeLettersCornerCommutators.new(results_model, known_letters)
 
 found = results_model.results.length
 missing = generator.class::VALID_PAIRS.length - found
