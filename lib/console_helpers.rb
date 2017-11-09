ee26c731ff8a6e6fe038885da48f30f751886d0b
Bernhard Brodowsky <bernhardb@tiger.zrh.corp.google.com>
2017-11-09 19:48:17 +0100

    Only start espeak once.

--- lib/console_helpers.rb
+++ lib/console_helpers.rb
@@ -5,10 +5,19 @@ module ConsoleHelpers
   def camel_to_snake(string)
     string.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').gsub(/([a-z\d])([A-Z])/,'\1_\2').tr("-", "_").downcase
   end
+
+  def espeak_processes
+    @espeak_processes ||= {}
+  end
+
+  def espeak_process(language)
+    espeak_processes[language] ||=
+      IO.popen("espeak -v #{language} -s 120", 'w+')
+  end
     
   def puts_and_say(stuff, language='de')
     puts stuff
-    system("echo '#{stuff}' | espeak -v #{language} -s 120")
+    espeak_process(language).puts(stuff)
   end
 
   def wait_for_any_key
