82ba310aa94e53c52edb8279f0a3452361f3fd75
Bernhard F. Brodowsky <bernhardb@google.com>
2017-11-16 18:58:57 -0800

    new letters

--- bin/corner_commutators.rb
+++ bin/corner_commutators.rb
@@ -14,8 +14,8 @@ include ConsoleHelpers
 # TODO Do this in the UI.
 
 results_model = ResultsModel.new(:corner_commutators)
-known_letters = ['a', 'b', 'd', 'e', 'j', 'g', 'i', 'm', 'r']
-new_letters = ['l']
+known_letters = ['a', 'b', 'd', 'e', 'j', 'g', 'i', 'm', 'r', 'l']
+new_letters = ['f']
 generator = SomeLettersCornerCommutators.new(results_model, known_letters)
 
 found = results_model.results.length
