4106ea11fa022999f5da18e75ff8fed694215d82
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2017-12-26 21:42:07 +0100

    Used single quotes

--- lib/options.rb
+++ lib/options.rb
@@ -4,29 +4,29 @@ require 'ostruct'
 class Options
   CommutatorInfo = Struct.new(:result_symbol, :generator_class)
   COMMUTATOR_TYPES = {
-    "corners" => CommutatorInfo.new(:corner_commutators, CornerCommutators),
-    "edges" => CommutatorInfo.new(:edge_commutators, EdgeCommutators),
-    "wings" => CommutatorInfo.new(:wing_commutators, WingCommutators),
-    "xcenters" => CommutatorInfo.new(:xcenter_commutators, XCenterCommutators),
-    "tcenters" => CommutatorInfo.new(:tcenter_commutators, TCenterCommutators)
+    'corners' => CommutatorInfo.new(:corner_commutators, CornerCommutators),
+    'edges' => CommutatorInfo.new(:edge_commutators, EdgeCommutators),
+    'wings' => CommutatorInfo.new(:wing_commutators, WingCommutators),
+    'xcenters' => CommutatorInfo.new(:xcenter_commutators, XCenterCommutators),
+    'tcenters' => CommutatorInfo.new(:tcenter_commutators, TCenterCommutators)
   }
   
   def self.parse(args)
     options = OpenStruct.new
-    options.commutator_info = COMMUTATOR_TYPES["corners"] 
+    options.commutator_info = COMMUTATOR_TYPES['corners'] 
     opt_parser = OptionParser.new do |opts|
-      opts.separator ""
-      opts.separator "Specific options:"      
-      opts.on("-c", "--commutator_type TYPE", COMMUTATOR_TYPES, "Use the given type of commutators for training.") do |info|
+      opts.separator ''
+      opts.separator 'Specific options:'      
+      opts.on('-c', '--commutator_type TYPE', COMMUTATOR_TYPES, 'Use the given type of commutators for training.') do |info|
         options.commutator_info = info
       end
 
-      opts.on("-r", "--restrict LETTERLIST", /[a-xA-x]+/, "List of letters to which the commutators should be restricted.",
-              "  (Only uses commutators that contain at least one of the given letters)") do |letters|
-        options.restrict_letters = letters.downcase.split("")
+      opts.on('-r', '--restrict LETTERLIST', /[a-xA-x]+/, 'List of letters to which the commutators should be restricted.',
+              '  (Only uses commutators that contain at least one of the given letters)') do |letters|
+        options.restrict_letters = letters.downcase.split('')
       end
       
-      opts.on_tail("-h", "--help", "Show this message") do
+      opts.on_tail('-h', '--help', 'Show this message') do
         puts opts
         exit
       end   
