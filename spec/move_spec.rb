91a1361811ba59b2691b1772f4b306c9aaae420f
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2019-09-10 22:14:01 +0200

    Improved parsing logic further and made parse_algorithm generally available

--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -9,13 +9,4 @@ describe Move do
     expect(parse_move('E').invert).to be == parse_move('E\'')
     expect(parse_move('E\'').invert).to be == parse_move('E')
   end
-
-  it 'should compute cancellations correctly' do
-    expect(parse_move('M').invert).to be == parse_move('M\'')
-    expect(parse_move('M\'').invert).to be == parse_move('M')
-    expect(parse_move('S').invert).to be == parse_move('S\'')
-    expect(parse_move('S\'').invert).to be == parse_move('S')
-    expect(parse_move('E').invert).to be == parse_move('E\'')
-    expect(parse_move('E\'').invert).to be == parse_move('E')
-  end
 end
