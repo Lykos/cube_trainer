a50812f73b1be421b4db62fb4de42857e1939267
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2019-12-26 02:00:54 +0100

    Added forgotten input item

---
+++ lib/input_item.rb
@@ -0,0 +1,18 @@
+module CubeTrainer
+
+  class InputItem
+    
+    def initialize(representation, cube_state=nil)
+      @representation = representation
+      @cube_state = cube_state
+    end
+
+    # The representation of this input item. This is a short thing that is the identity of the input item. This can be a letter pair or an alg name.
+    attr_reader :representation
+
+    # An (optional) cube state that represents this input item.
+    attr_reader :cube_state
+
+  end
+  
+end
