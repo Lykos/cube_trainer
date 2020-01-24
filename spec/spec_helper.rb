686c35e3241a430ee21ebbabe33442ff207e0973
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-01-24 03:45:41 +0100

    Added coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
+require 'simplecov'
+
+# This must be before we require cube_trainer.
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 require 'cube_trainer'
 
 include CubeTrainer
 
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.example_status_persistence_file_path = 'spec/examples.txt'
 end
