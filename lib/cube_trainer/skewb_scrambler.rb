e4a0c0a4fbbb32ad4c14369128545fc84c649923
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-02-11 05:08:33 +0100

    Fixed forgotten skewb scrambler

--- lib/cube_trainer/skewb_scrambler.rb
+++ lib/cube_trainer/skewb_scrambler.rb
@@ -1,23 +1,23 @@
-require 'cube_trainer/move'
-require 'cube_trainer/algorithm'
+require 'cube_trainer/core/algorithm'
+require 'cube_trainer/core/move'
 
 module CubeTrainer
 
   class SkewbScrambler
     
     def random_move(last_move)
-      (FixedCornerSkewbMove::ALL - [last_move, last_move.inverse]).sample
+      (Core::FixedCornerSkewbMove::ALL - [last_move, last_move.inverse]).sample
     end
 
     # TODO Make it random state!
     def random_moves(n)
       raise Argumenterror unless n.is_a?(Integer) && n >= 0
       return [] if n == 0
-      a = [FixedCornerSkewbMove::ALL.sample]
+      a = [Core::FixedCornerSkewbMove::ALL.sample]
       (n-1).times do
         a.push(random_move(a[-1]))
       end
-      Algorithm.new(a)
+      Core::Algorithm.new(a)
     end
     
   end
