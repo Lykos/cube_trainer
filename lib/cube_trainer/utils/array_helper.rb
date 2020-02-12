d66927ee4523fa27746e0ab1ee26561c8b4dd257
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-02-12 10:51:10 +0100

    Simplified arrayhelper

--- lib/cube_trainer/utils/array_helper.rb
+++ lib/cube_trainer/utils/array_helper.rb
@@ -23,6 +23,42 @@ module CubeTrainer
         false
       end
 
+      def next_state_after_nil(array, state)
+        case state
+        when :start
+          state = :first_nil_part
+        when :first_part
+          state = :nil_middle
+        when :middle
+          state = :second_nil_part
+        when :first_nil_part, :second_nil_part, :nil_middle
+          state = state
+        when :second_part
+          raise ArgumentError, "Cannot rotate out nils for #{array.inspect} since the nils are not contiguous."
+        else
+          raise "Unknown state #{state} reached."
+        end
+      end
+      
+      def next_state_after_non_nil(array, state)
+        case state
+        when :start
+          :first_part
+        when :first_nil_part
+          :middle
+        when :nil_middle
+          :second_part
+        when :first_part, :second_part, :middle
+          state
+        when :second_nil_part
+          raise ArgumentError, "Cannot rotate out nils for #{array.inspect} since the nils are not contiguous."
+        else
+          raise "Unknown state #{state} reached."
+        end
+      end
+
+      private :next_state_after_nil, :next_state_after_non_nil
+
       def rotate_out_nils(array)
         first_part = []
         second_part = []
@@ -31,35 +67,9 @@ module CubeTrainer
         state = :start
         array.each do |a|
           if a.nil?
-            case state
-            when :start
-              state = :first_nil_part
-            when :first_part
-              state = :nil_middle
-            when :middle
-              state = :second_nil_part
-            when :first_nil_part, :second_nil_part, :nil_middle
-              nil
-            when :second_part
-              raise ArgumentError, "Cannot rotate out nils for #{array.inspect} since the nils are not contiguous."
-            else
-              raise "Unknown state #{state} reached."
-            end
+            state = next_state_after_nil(array, state)
           else
-            case state
-            when :start
-              state = :first_part
-            when :first_nil_part
-              state = :middle
-            when :nil_middle
-              state = :second_part
-            when :first_part, :second_part, :middle
-              nil
-            when :second_nil_part
-              raise ArgumentError, "Cannot rotate out nils for #{array.inspect} since the nils are not contiguous."
-            else
-              raise "Unknown state #{state} reached."
-            end
+            state = next_state_after_non_nil(array, state)
             current_part = state == :second_part ? second_part : first_part
             current_part.push(a)
           end
