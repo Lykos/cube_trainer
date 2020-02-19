d613b138a1e56e9a85f7f953fb35bc23ea2e96a6
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2020-02-19 17:40:52 +0100

    Fixed comment of CompiledAlgorithm

--- lib/cube_trainer/core/compiled_algorithm.rb
+++ lib/cube_trainer/core/compiled_algorithm.rb
@@ -4,7 +4,7 @@ require 'cube_trainer/core/reversible_applyable'
 
 module CubeTrainer
   module Core
-    # Wrapper of the native C implementation of a compiled algorithm for a particular cube size.
+    # Base class for a compiled algorithm for a particular puzzle.
     class CompiledAlgorithm
       include ReversibleApplyable
 
