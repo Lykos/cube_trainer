3d34711c669eabc0724cd7aa8016816c8b0108a9
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-02-22 11:33:54 +0100

    Added tests for real

---
+++ lib/cube_trainer/anki/skewb_layer_anki_generator.rb
@@ -0,0 +1,170 @@
+# frozen_string_literal: true
+
+require 'cube_trainer/core/cube'
+require 'cube_trainer/core/skewb_state'
+require 'cube_trainer/skewb_layer_classifier'
+require 'cube_trainer/skewb_layer_improver'
+require 'cube_trainer/skewb_layer_searcher'
+require 'cube_trainer/skewb_transformation_describer'
+require 'csv'
+
+module CubeTrainer
+  module Anki
+    # Generates an Anki deck for Skewb layer cases.
+    class SkewbLayerAnkiGenerator
+      TITLE_ROW = [
+        'case description',
+        'main alg',
+        'center_transformations',
+        'top_corner_transformations',
+        'alternative algs',
+        'name',
+        'tags'
+      ].freeze
+
+      TOP_CORNERS = Core::Corner::ELEMENTS.select { |c| c.face_symbols.first == :U }.freeze
+      BOTTOM_CORNERS = Core::Corner::ELEMENTS.select { |c| c.face_symbols.first == :D }.freeze
+      NON_BOTTOM_FACES = Core::Face::ELEMENTS.reject { |c| c.face_symbol == :D }.freeze
+
+      def initialize(options)
+        @options = options
+      end
+
+      def calculate
+        SkewbLayerSearcher.calculate(
+          @options.color_scheme,
+          @options.verbose,
+          @options.depth
+        ).map! do |algs|
+          algs.map! { |alg| layer_improver.improve_layer(alg) }
+        end
+      end
+
+      def layer_improver
+        @layer_improver ||= SkewbLayerImprover.new(
+          Core::Face::D,
+          @options.color_scheme
+        )
+      end
+
+      def report_solutions(solutions)
+        return unless verbose
+
+        puts
+        puts "#{solutions.length} solutions:"
+        puts
+
+        state = @options.color_scheme.solved_skewb_state
+        solutions.each do |algs|
+          algs.first.inverse.apply_temporarily_to(state) do
+            puts state.colored_to_s
+            puts algs
+            puts
+          end
+        end
+      end
+
+      def verbose
+        @options.verbose
+      end
+
+      def names
+        @names ||=
+          begin
+            names = {}
+
+            if @options.name_file
+              puts "Reading name TSV file #{@options.name_file}." if verbose
+              CSV.foreach(@options.name_file, col_sep: "\t") do |csv|
+                names[csv[0]] = csv[1]
+              end
+              puts "Read #{names.length} names." if verbose
+            end
+            names
+          end
+      end
+
+      def layer_describer
+        @layer_describer ||=
+          begin
+            letter_scheme = @options.layer_corners_as_letters ? @options.letter_scheme : nil
+            SkewbTransformationDescriber.new(
+              [], BOTTOM_CORNERS, :omit_staying, @options.color_scheme, letter_scheme
+            )
+          end
+      end
+
+      def center_describer
+        @center_describer ||= SkewbTransformationDescriber.new(
+          NON_BOTTOM_FACES, [], :omit_staying, @options.color_scheme
+        )
+      end
+
+      def top_corner_describer
+        @top_corner_describer ||=
+          begin
+            letter_scheme = @options.top_corners_as_letters ? @options.letter_scheme : nil
+            SkewbTransformationDescriber.new(
+              [], TOP_CORNERS, :show_staying, @options.color_scheme, letter_scheme
+            )
+          end
+      end
+
+      def layer_classifier
+        @layer_classifier ||= SkewbLayerClassifier.new(
+          Core::Face::D,
+          @options.color_scheme
+        )
+      end
+
+      def name(source_descriptions)
+        name_letters =
+          source_descriptions.map(&:source).map do |p|
+            @options.letter_scheme.letter(p).capitalize
+          end
+        letter_pairs = name_letters.each_slice(2).map(&:join)
+        letter_pairs.map do |letter_pair|
+          key = letter_pair.length == 2 ? letter_pair : letter_pair * 2
+          names[key] || letter_pair
+        end.join(' & ')
+      end
+
+      def alg_row(algs)
+        main_alg = algs[0]
+        alternative_algs = algs[1..-1]
+        classification = layer_classifier.classify_layer(algs[0])
+        source_descriptions = layer_describer.source_descriptions(main_alg)
+        name = name(source_descriptions)
+        [
+          source_descriptions.join(', '),
+          main_alg.to_s,
+          center_describer.transformation_descriptions(main_alg).join(', '),
+          top_corner_describer.source_descriptions(main_alg).join(', '),
+          alternative_algs.join(', '),
+          name,
+          "#{main_alg.length}_mover #{classification}"
+        ]
+      end
+
+      def output(solutions)
+        return unless @options.output
+
+        puts if verbose
+
+        CSV.open(@options.output, 'wb', col_sep: "\t") do |csv|
+          csv << TITLE_ROW
+          solutions.each do |algs|
+            csv << alg_row(algs)
+          end
+        end
+        puts "Wrote #{solutions.length} items to TSV file #{@options.output}." if verbose
+      end
+
+      def run
+        solutions = calculate
+        report_solutions(solutions)
+        output(solutions)
+      end
+    end
+  end
+end
---
+++ spec/cube_trainer/anki/skewb_layer_anki_generator_spec.rb
@@ -0,0 +1,178 @@
+# frozen_string_literal: true
+
+require 'csv'
+require 'cube_trainer/anki/skewb_layer_anki_generator'
+require 'cube_trainer/color_scheme'
+require 'cube_trainer/letter_scheme'
+require 'tempfile'
+require 'ostruct'
+
+BASE_DECK = [
+  ['case description', 'main alg', 'center_transformations', 'top_corner_transformations', 'alternative algs', 'name', 'tags'],
+  ['', '', '', 'UFL stays, URF stays, ULB stays, UBR stays', '', '', '0_mover 4_solved'],
+  ['FUR → DRB', "UBR'", 'U → R → B', 'UFL stays, BUL → URF, BDR → ULB, BRU → UBR', '', 'E', '1_mover 3_solved'],
+  ['LUF → DFR, URF → DRB', "URF' UBR'", 'U → F → B', 'RUB → UFL, BUL → URF, BDR → ULB, DFR → UBR', '', 'MB', '2_mover 2_adjacent_solved'],
+  ['BRU → DFR, RFU → DRB', "URF UBR'", 'U ↔ B, F ↔ R', 'FRD → UFL, BUL → URF, BDR → ULB, LUF → UBR', '', 'RJ', '2_mover 2_adjacent_solved'],
+  ['LFD → DRB, BUL → DLF', "UFL' UBR'", 'U → L → F → R → B', 'FLU → UFL, RFU → URF, BDR → ULB, BRU → UBR', '', 'NQ', '2_mover 2_opposite_solved'],
+  ['ULB → DRB, RFU → DLF', "UFL UBR'", 'U → F → L → R → B', 'LUF → UFL, DLF → URF, BDR → ULB, BRU → UBR', '', 'CJ', '2_mover 2_opposite_solved'],
+  ['FUR → DRB, RUB → DBL', "ULB' UBR'", 'R → B → L', 'LDB → UFL, ULB → URF, BDR → ULB, UFL → UBR', '', 'EI', '2_mover 2_adjacent_solved'],
+  ['FUR → DRB, FLU → DBL', "ULB UBR'", 'U ↔ L, R ↔ B', 'BRU → UFL, LBU → URF, BDR → ULB, LDB → UBR', '', 'EG', '2_mover 2_adjacent_solved']
+].map(&:freeze).freeze
+
+def delete_columns(deck, columns)
+  deck.map! do |row|
+    row.reject.with_index { |e, i| columns.include?(i) }
+  end
+end
+
+RSpec::Matchers.define(:be_modified_deck) do |column, expected_elements, wildcard_columns=[]|
+  raise ArgumentError unless BASE_DECK.length == expected_elements.length
+  raise ArgumentError if wildcard_columns.include?(column)
+
+  expected =
+    BASE_DECK.zip(expected_elements).map do |row, element|
+      dupped = row.dup
+      dupped[column] = element
+      dupped
+    end
+  delete_columns(expected, wildcard_columns)
+
+  match do |actual|
+    dupped_actual = actual.dup
+    delete_columns(dupped_actual, wildcard_columns)
+    dupped_actual == expected
+  end
+
+  failure_message do |actual|
+    dupped_actual = actual.dup
+    delete_columns(dupped_actual, wildcard_columns)
+    actual_elements = dupped_actual.map { |row| row[column] }
+    if expected_elements != actual_elements
+      "expected that changed column #{column} with elements #{actual_elements.inspect} would equal #{expected_elements.inspect}"
+    else
+      "expected that #{dupped_actual.inspect} would equal #{expected.inspect}"
+    end
+  end
+end
+
+describe Anki::SkewbLayerAnkiGenerator do
+  let(:output) { Tempfile.new(['deck', '.tsv']) }
+  let(:base_options) do
+    options = OpenStruct.new
+    options.color_scheme = ColorScheme::WCA
+    options.output = output.path
+    options.depth = 2
+    options.letter_scheme = BernhardLetterScheme.new
+    options
+  end
+  let(:options) { base_options }
+  let(:generator) { described_class.new(options) }
+  let(:deck) { CSV.read(output, col_sep: "\t") }
+
+  it 'generates the right deck' do
+    generator.run
+
+    expect(deck).to eq(BASE_DECK)
+  end
+
+  context 'when layer_corners_as_letters is turned on' do
+    let(:options) do
+      options = base_options
+      options.layer_corners_as_letters = true
+      options
+    end
+
+    it 'uses letters for layer corners' do
+      generator.run
+
+      expect(deck).to be_modified_deck(
+        0, [
+          'case description',
+          '',
+          'E → V',
+          'M → U, B → V',
+          'R → U, J → V',
+          'N → V, Q → W',
+          'C → V, J → W',
+          'E → V, I → X',
+          'E → V, G → X'
+        ]
+      )
+    end
+
+    context 'when there is a name_file' do
+      let(:name_file) { 'testdata/name_file.tsv' }
+      let(:options) do
+        options = base_options
+        options.layer_corners_as_letters = true
+        options.name_file = name_file
+        options
+      end
+
+
+      it 'uses letters for layer corners' do
+        generator.run
+    
+        expect(deck).to be_modified_deck(
+          0, [
+            'case description',
+            '',
+            'E → V',
+            'M → U, B → V',
+            'R → U, J → V',
+            'N → V, Q → W',
+            'C → V, J → W',
+            'E → V, I → X',
+            'E → V, G → X'
+          ],
+          [5]
+        )
+      end
+
+      it 'uses transformed names' do
+        generator.run
+
+        expect(deck).to be_modified_deck(
+          5, [
+            'name',
+            '',
+            'elektro',
+            'miriam barner',
+            'raj',
+            'naqter',
+            'cuji',
+            'elfin',
+            'egg'
+          ],
+          [0]
+        )
+      end
+    end
+  end
+
+  context 'when top_corners_as_letters is turned on' do
+    let(:options) do
+      options = base_options
+      options.top_corners_as_letters = true
+      options
+    end
+
+    it 'uses letters for layer corners' do
+      generator.run
+
+      expect(deck).to be_modified_deck(
+        3, [
+          'top_corner_transformations',
+          'A stays, B stays, C stays, D stays',
+          'A stays, Q → B, T → C, R → D',
+          'I → A, Q → B, T → C, U → D',
+          'F → A, Q → B, T → C, M → D',
+          'G → A, J → B, T → C, R → D',
+          'M → A, W → B, T → C, R → D',
+          'P → A, C → B, T → C, A → D',
+          'R → A, O → B, T → C, P → D'
+        ]
+      )
+    end
+  end
+end
