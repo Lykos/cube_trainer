4a84b564ff4f6af2c7f630ac074829267d151671
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-03-03 00:46:02 +0100

    Sometimes copy, sometimes go and reverse for apply_temporarily_to

--- lib/cube_trainer/core/reversible_applyable.rb
+++ lib/cube_trainer/core/reversible_applyable.rb
@@ -1,17 +1,39 @@
 # frozen_string_literal: true
 
+require 'cube_trainer/core/skewb_state'
+
 module CubeTrainer
   module Core
     # Module that makes a class that has an `apply_to` and `reverse` be able to apply temporarily.
     module ReversibleApplyable
-      def apply_to_dupped(cube_state)
-        dupped = cube_state.dup
+      def apply_to_dupped(puzzle_state)
+        dupped = puzzle_state.dup
         apply_to(dupped)
         dupped
       end
 
-      def apply_temporarily_to(cube_state)
-        yield apply_to_dupped(cube_state)
+      # Applies the current algorithm/cycle/whatever to the given puzzle state and yields the
+      # modified version. The puzzle state will be the same as the original after this function
+      # returns.
+      # Whether the yielded puzzle state is actually the same as the passed one or a copy is an
+      # implementation detail.
+      def apply_temporarily_to(puzzle_state)
+        if with_dup_is_faster?(puzzle_state)
+          return yield apply_to_dupped(puzzle_state)
+        end
+
+        apply_to(puzzle_state)
+        begin
+          yield puzzle_state
+        ensure
+          inverse.apply_to(puzzle_state)
+        end
+      end
+
+      private
+
+      def with_dup_is_faster?(state)
+        !state.is_a?(CubeState) || state.n <= 4
       end
     end
   end
