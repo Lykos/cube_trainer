7168637fee9c26d4339bb63cc94a1e2478f67c3d
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-03-17 20:16:21 +0100

    Unified coverage scorers

--- lib/cube_trainer/training/coverage_scorer.rb
+++ lib/cube_trainer/training/coverage_scorer.rb
@@ -4,22 +4,45 @@ require 'cube_trainer/training/abstract_scorer'
 
 module CubeTrainer
   module Training
-    # A scorer that prefers items that haven't been shown in a while.
-    class CoverageScorer < AbstractScorer
+    # Abstract class for a scorer that prefers items that haven't been shown in a while.
+    class AbstractCoverageScorer < AbstractScorer
       def extra_info(input_item)
-        "items since last occurrence #{@result_history.items_since_last_occurrence(input_item)}"
+        "items since last occurrence #{@result_history.items_since_last_occurrence(input_item)}" \
+        "days since last occurrence #{@result_history.last_occurrence_days_ago(input_item)}"
       end
 
-      # A score that prefers items that haven't been shown in a while.
       def score(input_item)
-        index = @result_history.items_since_last_occurrence(input_item)
-        return 0 if index.nil?
+        long_ago_metric = long_ago_metric(input_item)
+        return 0 if long_ago_metric.nil? || long_ago_metric.zero?
 
-        index**@config[:index_exponent]
+        long_ago_metric**@config[exponent_config_key]
       end
 
       def color_symbol
         :yellow
+      end
+    end
+
+    # A scorer that prefers items that haven't been shown in a while, i.e. with lots of items in
+    # between.
+    class CoverageScorer < AbstractCoverageScorer
+      def long_ago_metric(input_item)
+        @result_history.items_since_last_occurrence(input_item)
+      end
+
+      def exponent_config_key
+        :index_exponent
+      end
+    end
+
+    # A scorer that prefers items that haven't been shown in a lot of training days.
+    class DaysCoverageScorer < AbstractCoverageScorer
+      def long_ago_metric(input_item)
+        @result_history.last_occurrence_days_ago(input_item)
+      end
+
+      def exponent_config_key
+        :days_ago_exponent
       end
     end
   end
