8921904d7518ec1fe9f025b5e437d938901830f1
Bernhard Brodowsky <brodowsb@ethz.ch>
2020-03-22 11:03:04 +0100

    Added rubocop

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 source 'https://rubygems.org' do
+  gem 'activerecord'
   gem 'colorize'
   gem 'parallel'
   gem 'ruby-filemagic'
@@ -9,7 +10,6 @@ source 'https://rubygems.org' do
   gem 'sqlite3'
   gem 'wombat'
   gem 'xdg'
-  gem 'activerecord'
 
   group :development, :test do
     gem 'rake'
--- bin/migrate.rb
+++ bin/migrate.rb
@@ -8,10 +8,8 @@ require 'active_record'
 require 'cube_trainer/training/results_persistence'
 require '20200320214513_add_success_num_hints_columns'
 
-# TODO Refactor this file to only include the migration itself and find a proper way for migrations
+# TODO: Refactor this file to only include the migration itself and find a proper way for migrations
 
 db_file = CubeTrainer::Training::ResultsPersistence::DBConnectionHelper.new.db_file
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: db_file)
 AddSuccessNumHintsColumns.migrate(:up)
-
-
