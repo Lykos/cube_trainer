32d559fcf761e022cfeb4d0c082101f93222dac3
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-03-25 20:36:38 +0100

    Fixed bugs

--- app/controllers/timer_controller.rb
+++ app/controllers/timer_controller.rb
@@ -1,20 +1,21 @@
 require 'cube_trainer/training/commutator_options'
+require 'cube_trainer/training/results_model'
 require 'ostruct'
 
 class TimerController < ApplicationController
   OPTIONS = begin
               options = CubeTrainer::Training::CommutatorOptions.default_options
-              options.commutator_info = CubeTrainer::Training::CommutatorOptions::COMMUTATOR_INFOS[:corners] || raise
+              options.commutator_info = CubeTrainer::Training::CommutatorOptions::COMMUTATOR_TYPES[:corners] || raise
               options
             end
   MODE = :ulb_corner_commutators
 
   def index
-    logger.error "asf"
     generator = OPTIONS.commutator_info.generator_class.new(OPTIONS)
+    results_model = CubeTrainer::Training::ResultsModel.new(MODE)
     input_sampler = generator.input_sampler(results_model)
     @input_item = input_sampler.random_item
-    Input.new(mode: MODE, input_representation: @input_item.representation).save!
+    CubeTrainer::Training::Input.new(mode: MODE, input_representation: @input_item.representation).save!
   end
 
   def store
