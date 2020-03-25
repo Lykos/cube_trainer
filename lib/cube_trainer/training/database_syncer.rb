a4b60cf2a98dcba44582f1bccd6b754ef2ec23d9
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-03-25 13:12:47 +0100

    Only upload non-existing stuff

--- lib/cube_trainer/training/database_syncer.rb
+++ lib/cube_trainer/training/database_syncer.rb
@@ -20,25 +20,19 @@ module CubeTrainer
       end
 
       def upload!
-        puts "Fetching uploads."
-        uploaded = fetch_uploaded
-        now = Time.now
-        puts "Uploading #{uploaded.length} records of type #{@model.name}."
-        uploaded.each { |item| item.uploaded_at = now }
-        upload(uploaded)
-        puts "Saving updated uploaded_at timestamps."
-        progress_bar = ProgressBar.create(title: 'Saved', total: uploaded.length)
-        ActiveRecord::Base.connected_to(database: :primary) do
-          uploaded.each do |u|
-            u.save(touch: false)
-            progress_bar.increment
-          end
+        puts 'Fetching uploads.'
+        to_upload = fetch_to_upload
+        if to_upload.empty?
+          puts 'Nothing to upload.'
+          return
         end
-        puts "Saved updated uploaded_at timestamps."
+
+        upload(to_upload)
+        save_uploaded_at(to_upload)
       end
 
       def download!
-        puts "Fetching downloads."
+        puts 'Fetching downloads.'
         download_state = fetch_download_state
         now = Time.now
         downloaded = fetch_downloaded(download_state, now)
@@ -58,7 +52,7 @@ module CubeTrainer
 
       private
 
-      def fetch_uploaded
+      def fetch_to_upload
         ActiveRecord::Base.connected_to(database: :primary) do
           @model.where(
             'hostname = ? AND (uploaded_at IS NULL OR updated_at > uploaded_at)',
@@ -67,14 +61,40 @@ module CubeTrainer
         end
       end
 
-      def upload(uploaded)
-        stuff = uploaded.map do |u|
-          u = u.dup
-          u.id = nil
-          u
+      def save_uploaded_at(uploaded)
+        puts 'Saving updated uploaded_at timestamps.'
+        progress_bar = ProgressBar.create(title: 'Saved', total: uploaded.length)
+        ActiveRecord::Base.connected_to(database: :primary) do
+          uploaded.each do |u|
+            u.save(touch: false)
+            progress_bar.increment
+          end
         end
+      end
+
+      def copy_attributes
+        u.attributes.each do |k, v|
+          next if %w[hostname created_at id].include?(k)
+
+          uploaded_u.send('${k}=', v)
+        end
+      end
+
+      def upload(to_upload)
+        puts "Uploading #{to_upload.length} records of type #{@model.name}."
+        progress_bar = ProgressBar.create(title: 'Uploaded', total: to_upload.length)
         ActiveRecord::Base.connected_to(database: :global) do
-          @model.import(stuff)
+          to_upload.each do |u|
+            u.uploaded_at = Time.now
+            uploaded_u = @model.create_or_find_by!(
+              hostname: u.hostname,
+              mode: u.mode,
+              created_at: u.created_at
+            )
+            copy_attributes(from: u, to: uploaded_u)
+            u.save!
+            progress_bar.increment
+          end
         end
       end
 
