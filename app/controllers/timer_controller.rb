85c0a8235f8bfdf1dcbd8be56712e53d1e888d6c
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-03-25 20:52:47 +0100

    Support for post

--- app/controllers/timer_controller.rb
+++ app/controllers/timer_controller.rb
@@ -14,12 +14,17 @@ class TimerController < ApplicationController
     generator = OPTIONS.commutator_info.generator_class.new(OPTIONS)
     results_model = CubeTrainer::Training::ResultsModel.new(MODE)
     input_sampler = generator.input_sampler(results_model)
+    @results = results_model.results
     @input_item = input_sampler.random_item
-    CubeTrainer::Training::Input.new(mode: MODE, input_representation: @input_item.representation).save!
+    @input = CubeTrainer::Training::Input.new(mode: MODE, input_representation: @input_item.representation)
+    @input.save!
   end
 
-  def store
-    Result.from_input_and_partial(Input.find(params[:id]), PartialResult.new(params[:time_s]))
-    redirect_to :index
+  def post
+    @input = CubeTrainer::Training::Input.find(params[:id])
+    partial_result = CubeTrainer::Training::PartialResult.new(params[:time_s])
+    CubeTrainer::Training::Result.from_input_and_partial(@input, partial_result)
+    @input.destroy!
+    redirect_to '/timer/index'
   end
 end
