ba1b925242fb5946324de4002f05ef7023e48794
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-03-25 21:42:45 +0100

    Made start stop stuff work

--- app/controllers/timer_controller.rb
+++ app/controllers/timer_controller.rb
@@ -11,20 +11,30 @@ class TimerController < ApplicationController
   MODE = :ulb_corner_commutators
 
   def index
+    @results = CubeTrainer::Training::Result.where(mode: MODE)
+  end
+
+  def start
     generator = OPTIONS.commutator_info.generator_class.new(OPTIONS)
     results_model = CubeTrainer::Training::ResultsModel.new(MODE)
     input_sampler = generator.input_sampler(results_model)
-    @results = results_model.results
+    @results = CubeTrainer::Training::Result.where(mode: MODE)
+    logger.info @results.length
     @input_item = input_sampler.random_item
     @input = CubeTrainer::Training::Input.new(mode: MODE, input_representation: @input_item.representation)
     @input.save!
   end
 
-  def post
+  def delete
+    CubeTrainer::Training::Result.find(params[:id]).destroy!
+    redirect_to '/timer/index'
+  end
+
+  def stop
     @input = CubeTrainer::Training::Input.find(params[:id])
     partial_result = CubeTrainer::Training::PartialResult.new(params[:time_s])
-    CubeTrainer::Training::Result.from_input_and_partial(@input, partial_result)
+    CubeTrainer::Training::Result.from_input_and_partial(@input, partial_result).save!
     @input.destroy!
-    redirect_back fallback_location: '/timer/index'
+    redirect_to '/timer/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'timer/index'
-  post 'timer/post'
+  post 'timer/start'
+  post 'timer/stop'
+  post 'timer/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
