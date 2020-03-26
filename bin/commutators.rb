823872fb382a283c7d0b26bb5f474239ac395bb3
Bernhard Brodowsky <bernhardb@google.com>
2020-03-26 13:13:40 +0100

    Use the right primary database

--- bin/commutators.rb
+++ bin/commutators.rb
@@ -3,26 +3,32 @@
 require 'cube_trainer/buffer_helper'
 require 'cube_trainer/training/commutator_sets'
 require 'cube_trainer/training/commutator_options'
+require 'cube_trainer/training/database_syncer'
 require 'cube_trainer/training/results_model'
 require 'cube_trainer/training/stats_computer'
 require 'cube_trainer/training/trainer'
 
 options = CubeTrainer::Training::CommutatorOptions.parse(ARGV)
-results_model = CubeTrainer::Training::ResultsModel.new(
-  CubeTrainer::BufferHelper.mode_for_options(options)
-)
-generator = options.commutator_info.generator_class.new(options)
-hinter = generator.hinter
-learner = options.commutator_info.learner_class.new(hinter, results_model, options)
-stats_computer = CubeTrainer::Training::StatsComputer.new(Time.now, options)
+syncer = CubeTrainer::Training::DatabaseSyncer.new(CubeTrainer::Training::Result)
+syncer.download!
+at_exit { syncer.upload! }
+ActiveRecord::Base.connected_to(database: :primary) do
+  results_model = CubeTrainer::Training::ResultsModel.new(
+    CubeTrainer::BufferHelper.mode_for_options(options)
+  )
+  generator = options.commutator_info.generator_class.new(options)
+  hinter = generator.hinter
+  learner = options.commutator_info.learner_class.new(hinter, results_model, options)
+  stats_computer = CubeTrainer::Training::StatsComputer.new(Time.now, options)
 
-if generator.input_items
-  stats = stats_computer.input_stats(generator.input_items)
-  puts "#{stats[:found]} of #{stats[:total]} items found, #{stats[:newish_elements]} of them " \
-       "newish, #{stats[:missing]} missing."
-  puts "#{stats_computer.num_results} results, #{stats_computer.num_recent_results} of them in " \
-       'the last 24 hours.'
+  if generator.input_items
+    stats = stats_computer.input_stats(generator.input_items)
+    puts "#{stats[:found]} of #{stats[:total]} items found, #{stats[:newish_elements]} of them " \
+         "newish, #{stats[:missing]} missing."
+    puts "#{stats_computer.num_results} results, #{stats_computer.num_recent_results} of them in " \
+         'the last 24 hours.'
+  end
+
+  input_sampler = generator.input_sampler(results_model)
+  CubeTrainer::Training::Trainer.new(learner, results_model, input_sampler).run
 end
-
-input_sampler = generator.input_sampler(results_model)
-CubeTrainer::Training::Trainer.new(learner, results_model, input_sampler).run
