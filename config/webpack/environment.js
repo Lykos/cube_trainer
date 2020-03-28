62a26a5f6298b01c2b9083882310499fb467ad35
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-03-28 08:01:49 +0100

    Added angular

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,17 @@ const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 
 environment.loaders.prepend('typescript', typescript)
+environment.loaders.append('html', {
+  test: /\.html$/,
+  use: [{
+    loader: 'html-loader',
+    options: {
+      minimize: true,
+      removeAttributeQuotes: false,
+      caseSensitive: true,
+      customAttrSurround: [ [/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/] ],
+      customAttrAssign: [ /\)?\]?=/ ]
+    }
+  }]
+})
 module.exports = environment
