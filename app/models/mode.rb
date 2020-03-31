9c9ada67626866b57920f96be5cd395533dbe2b6
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-03-31 23:30:01 +0200

    Added defaults to mode

--- app/models/mode.rb
+++ app/models/mode.rb
@@ -1,2 +1,13 @@
 class Mode < ApplicationRecord
+  belongs_to :user
+
+  # TODO: Make it configurable
+  def letter_scheme
+    @letter_scheme ||= BernhardLetterScheme.new
+  end
+
+  # TODO: Make it configurable
+  def color_scheme
+    ColorScheme::BERNHARD
+  end
 end
