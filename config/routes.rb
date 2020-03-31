2f0c8fc9aff7a322cd14d0d7199613ef2635987f
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-03-31 22:13:17 +0200

    Added routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#logout'
+  get 'welcome', to: 'sessions#welcome'
   resources :users
   get 'timer/index'
   post 'timer/next_input'
