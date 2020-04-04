eaa9774f7f96d590b1e59c91f6ba7acdbc92ee17
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-04 02:54:51 +0200

    Made modes users/modes a hierarchy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :modes
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   post 'logout', to: 'sessions#logout'
   get 'welcome', to: 'sessions#welcome'
   root 'sessions#welcome'
-  resources :users
+  resources :users do
+    resources :modes
+  end
   get 'training/:mode_id', to: 'timer#index', as: :timer
   post 'training/:mode_id/next_input', to: 'timer#next_input'
   post 'training/:mode_id/stop', to: 'timer#stop'
