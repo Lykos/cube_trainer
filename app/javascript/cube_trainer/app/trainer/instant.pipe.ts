713338095f3e17be1a9553986df8b1196a9a1b28
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-05 02:01:42 +0200

    Nicer date format

--- app/javascript/cube_trainer/app/trainer/instant.pipe.ts
+++ app/javascript/cube_trainer/app/trainer/instant.pipe.ts
@@ -1,11 +1,14 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform, Inject, LOCALE_ID } from '@angular/core';
 import { Instant } from '../utils/instant';
+import { formatDate } from '@angular/common';
 
 @Pipe({
   name: 'instant'
 })
 export class InstantPipe implements PipeTransform {
+  constructor(@Inject(LOCALE_ID) private readonly locale: string) {}
+
   transform(instant: Instant) {
-    return instant.toString();
+    return formatDate(instant.toDate(), 'short', this.locale);
   }
 }
