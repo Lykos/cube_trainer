e840e0df8ea72ab6fd7e8cb5f0652e9c8527c028
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-05 02:02:54 +0200

    Reformatted results_data_source

--- app/javascript/cube_trainer/app/trainer/results_data_source.ts
+++ app/javascript/cube_trainer/app/trainer/results_data_source.ts
@@ -6,28 +6,29 @@ import { ResultService } from './result.service';
 
 export class ResultsDataSource implements DataSource<Result> {
 
-    private resultsSubject = new BehaviorSubject<Result[]>([]);
-    private loadingSubject = new BehaviorSubject<boolean>(false);
+  private resultsSubject = new BehaviorSubject<Result[]>([]);
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  public loading$ = this.loadingSubject.asObservable();
 
-    public loading$ = this.loadingSubject.asObservable();
+  constructor(private readonly resultService: ResultService) {}
 
-    constructor(private readonly resultService: ResultService) {}
+  connect(collectionViewer: CollectionViewer): Observable<Result[]> {
+    return this.resultsSubject.asObservable();
+  }
 
-    connect(collectionViewer: CollectionViewer): Observable<Result[]> {
-        return this.resultsSubject.asObservable();
-    }
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.resultsSubject.complete();
+    this.loadingSubject.complete();
+  }
 
-    disconnect(collectionViewer: CollectionViewer): void {
-        this.resultsSubject.complete();
-        this.loadingSubject.complete();
-    }
-
-    loadResults(modeId: number) {
-        this.loadingSubject.next(true);
-        this.resultService.list(modeId).pipe(
-            catchError(() => of([])),
-            finalize(() => this.loadingSubject.next(false))
-        )
-        .subscribe((results: Result[]) => this.resultsSubject.next(results));
-    }    
+  loadResults(modeId: number) {
+    this.loadingSubject.next(true);
+    this.resultService.list(modeId).pipe(
+      catchError(() => of([])),
+      finalize(() => this.loadingSubject.next(false))
+    )
+      .subscribe((results: Result[]) => {
+	return this.resultsSubject.next(results);
+      });
+  }    
 }
