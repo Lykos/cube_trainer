c621bd024844c10e08b42d55a12984ac3b97c947
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-06 23:29:46 +0200

    Removed unneeded hello_angular

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module CubeTrainer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.hosts << 'jaguar'
+    config.hosts << 'localhost'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # Temporary
-  config.hosts << 'jaguar'
 end
