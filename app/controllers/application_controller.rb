0d6a715d20ed69a15f8b3546da24525cc169fe7b
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-08 00:54:57 +0200

    Removed useless helper_method for application controller. We don't have views and it's deprecated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@
 
 class ApplicationController < ActionController::Base
   before_action :authorized
-  helper_method :current_user
-  helper_method :logged_in?
 
   def current_user
     User.find_by(id: session[:user_id])
