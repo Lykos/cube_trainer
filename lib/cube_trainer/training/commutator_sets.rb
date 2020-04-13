bf30befc50ef7a06d0a12fec5349f22c16b17d2b
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-13 22:17:06 +0200

    Fixed commutator sets

--- lib/cube_trainer/training/commutator_sets.rb
+++ lib/cube_trainer/training/commutator_sets.rb
@@ -212,7 +212,7 @@ module CubeTrainer
         return NoHinter.new(input_items) unless corner_mode = @mode.used_mode(:corner_commutators)
 
         corner_hinter = CommutatorHintParser.maybe_parse_hints(PART_TYPE, corner_mode)
-        Corner3TwistHinter.new(corner_mode, corner_hinter, mode)
+        Corner3TwistHinter.new(corner_mode, corner_hinter, @mode)
       end
 
       def goal_badness
@@ -306,7 +306,7 @@ module CubeTrainer
 
           # Now we generate letter pairs
           extended_solutions.map do |s|
-            s.map { |comm| LetterPair.new(comm.map { |p| letter(p) }) }
+            s.map { |comm| LetterPair.new(comm.map { |p| @letter_scheme.letter(p) }) }
           end
         end
         # rubocop:enable Metrics/MethodLength
