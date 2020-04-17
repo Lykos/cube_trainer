531909ed55b82b91382093a5fb1dfc9b2c53995a
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-17 03:19:30 +0200

    added unique index for user email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_233747) do
+ActiveRecord::Schema.define(version: 2020_04_17_011824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2020_04_16_233747) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
     t.string "email", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
