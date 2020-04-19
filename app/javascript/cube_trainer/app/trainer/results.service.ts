14c795013f2a3b1018bb657811c7b6be2c6b3307
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-20 01:55:16 +0200

    Small refactoring

--- app/javascript/cube_trainer/app/trainer/results.service.ts
+++ app/javascript/cube_trainer/app/trainer/results.service.ts
@@ -7,25 +7,25 @@ import { Observable } from 'rxjs';
 import { seconds } from '../utils/duration'
 import { fromDateString } from '../utils/instant'
 
+function parseResult(result: any): Result {
+  return {
+    id: result.id,
+    timestamp: fromDateString(result.created_at),
+    duration: seconds(result.time_s),
+    inputRepresentation: result.input_representation,
+    numHints: result.num_hints,
+  };
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ResultsService {
   constructor(private readonly rails: RailsService) {}
 
-  parseResult(result: any): Result {
-    return {
-      id: result.id,
-      timestamp: fromDateString(result.created_at),
-      duration: seconds(result.time_s),
-      inputRepresentation: result.input_representation,
-      numHints: result.num_hints,
-    };
-  }
-
   list(modeId: number, offset: number, limit: number): Observable<Result[]> {
     return this.rails.ajax<any[]>(HttpVerb.Get, `/modes/${modeId}/results`, {offset, limit}).pipe(
-      map(results => results.map(this.parseResult)));
+      map(results => results.map(parseResult)));
   }
 
   destroy(modeId: number, resultId: number): Observable<void> {
