33e800fbf3d2a73721004487194907941a739f64
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-20 00:22:49 +0200

    Added active record type for stat type

---
+++ app/models/stat_type_type.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Active record type for stat_type.
+class StatTypeType < ActiveRecord::Type::String
+  def cast(value)
+    return if value.nil?
+    return value if value.is_a?(StatType)
+    raise TypeError unless value.is_a?(String) || value.is_a?(Symbol)
+
+    StatType.find_by!(key: value)
+  end
+
+  def serialize(value)
+    return if value.nil?
+
+    value = cast(value) unless value.is_a?(StatType)
+    value.key
+  end
+end
--- config/initializers/types.rb
+++ config/initializers/types.rb
@@ -3,8 +3,10 @@ require 'input_representation_type'
 require 'mode_type'
 require 'mode_type_type'
 require 'achievement_type'
+require 'stat_type_type'
 
 ActiveRecord::Type.register(:symbol, SymbolType)
 ActiveRecord::Type.register(:input_representation, InputRepresentationType)
 ActiveRecord::Type.register(:mode_type, ModeTypeType)
 ActiveRecord::Type.register(:achievement, AchievementType)
+ActiveRecord::Type.register(:stat_type, StatType)
