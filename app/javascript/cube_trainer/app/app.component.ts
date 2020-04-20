ec27f482db9589e4db766f608808c68ead46f2f1
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-20 17:30:37 +0200

    Added style for overall cube trainer

--- app/javascript/cube_trainer/app/app.component.ts
+++ app/javascript/cube_trainer/app/app.component.ts
@@ -4,8 +4,15 @@ import { Component } from '@angular/core';
   selector: 'cube-trainer',
   template: `
 <toolbar>Loading toolbar...</toolbar>
+<div class='cube-trainer-container'>
 <router-outlet></router-outlet>
-`
+</div>
+`,
+  styles: [`
+.cube-trainer-container {
+  margin: 20px;
+}
+`]
 })
 export class AppComponent {
 }
