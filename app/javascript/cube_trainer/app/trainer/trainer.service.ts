b46152906812fcb1db0a383986142e0762dae4b8
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-20 17:00:59 +0200

    Refactored trainer service

--- app/javascript/cube_trainer/app/trainer/trainer.service.ts
+++ app/javascript/cube_trainer/app/trainer/trainer.service.ts
@@ -5,6 +5,16 @@ import { HttpVerb } from '../rails/http_verb';
 import { PartialResult } from './partial-result';
 import { Mode } from '../modes/mode';
 import { ImgSide } from './img-side';
+import { Observable } from 'rxjs';
+
+function constructPath(modeId: number, input?: InputItem) {
+  const suffix = input ? `/${input.id}` : '';
+  return `/trainer/${modeId}/inputs${suffix}`;
+}
+
+function transformedPartialResult(partialResult: PartialResult) {
+  return {timeS: partialResult.duration.toSeconds()};
+}
 
 @Injectable({
   providedIn: 'root',
@@ -13,28 +23,19 @@ export class TrainerService {
   constructor(private readonly rails: RailsService) {}
 
   inputImgSrc(mode: Mode, input: InputItem, imgSide: ImgSide) {
-    return `${this.constructPath(mode.id, input)}/image/${imgSide}.jpg`
+    return `${constructPath(mode.id, input)}/image/${imgSide}.jpg`
   }
 
-  constructPath(modeId: number, input?: InputItem) {
-    const suffix = input ? `/${input.id}` : '';
-    return `/trainer/${modeId}/inputs${suffix}`;
-
+  create(modeId: number): Observable<InputItem> {
+    return this.rails.ajax<InputItem>(HttpVerb.Post, constructPath(modeId), {});
   }
 
-  create(modeId: number) {
-    return this.rails.ajax<InputItem>(HttpVerb.Post, this.constructPath(modeId), {});
+  destroy(modeId: number, input: InputItem): Observable<void> {
+    return this.rails.ajax<void>(HttpVerb.Delete, constructPath(modeId, input), {});
   }
 
-  destroy(modeId: number, input: InputItem) {
-    return this.rails.ajax<void>(HttpVerb.Delete, this.constructPath(modeId, input), {});
-  }
-
-  stop(modeId: number, input: InputItem, partialResult: PartialResult) {
-    return this.rails.ajax<void>(HttpVerb.Post, this.constructPath(modeId, input), {partialResult: this.transformedPartialResult(partialResult)});
-  }
-
-  transformedPartialResult(partialResult: PartialResult) {
-    return {timeS: partialResult.duration.toSeconds()};
+  stop(modeId: number, input: InputItem, partialResult: PartialResult): Observable<void> {
+    return this.rails.ajax<void>(HttpVerb.Post, constructPath(modeId, input),
+				 {partialResult: transformedPartialResult(partialResult)});
   }
 }
