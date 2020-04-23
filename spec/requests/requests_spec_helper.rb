c2f2288e3d2964ff75e8de078d93ec84ce286ff0
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-24 00:33:21 +0200

    Rubocop fixes

--- spec/requests/requests_spec_helper.rb
+++ spec/requests/requests_spec_helper.rb
@@ -8,22 +8,16 @@ end
 # * `login(username, password)`
 # * `login(email, password)`
 # * `login(user)`
-def post_login(username_or_email_or_user, password=nil)
+def post_login(username_or_email_or_user, password = nil)
   if password
     username_or_email = username_or_email_or_user
     post '/login',
-         params: {
-           username_or_email: username_or_email,
-           password: password
-         },
+         params: { username_or_email: username_or_email, password: password },
          headers: headers
   else
     user = username_or_email_or_user
     post '/login',
-         params: {
-           username_or_email: user.name,
-           password: user.password
-         },
+         params: { username_or_email: user.name, password: user.password },
          headers: headers
   end
 end
