258d64cc98d87c05e4c0c666a61a9ca9af5542f1
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-28 01:35:21 +0200

    Only create user if there are results

--- db/migrate/20200331184449_create_users.rb
+++ db/migrate/20200331184449_create_users.rb
@@ -3,6 +3,9 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     has_secure_password
   end
 
+  class Result < ApplicationRecord
+  end
+
   def change
     create_table :users do |t|
       t.string :name, null: false
@@ -12,27 +15,29 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     end
 
     user = nil
-    reversible do |dir|
-      dir.up do
-        User.reset_column_information
-        user =
-          User.create!(
-            name: 'result_owner',
-            password: OsHelper.default_password,
-            password_confirmation: OsHelper.default_password
-          )
-      end
-      dir.down do
-        # Nothing. The added data gets removed by the schema changes.
+    if Result.exists?
+      reversible do |dir|
+        dir.up do
+          User.reset_column_information
+          user =
+            User.create!(
+              name: 'result_owner',
+              password: OsHelper.default_password,
+              password_confirmation: OsHelper.default_password
+            )
+        end
+        dir.down do
+          # Nothing. The added data gets removed by the schema changes.
+        end
       end
     end
 
     add_column :cube_trainer_training_results, :user_id, :integer, default: user&.id, null: false
-    change_column :cube_trainer_training_results, :user_id, :integer, default: nil
+    change_column_default :cube_trainer_training_results, :user_id, from: user.id, to: nil if user
     add_index :cube_trainer_training_results, :user_id
 
     add_column :cube_trainer_training_inputs, :user_id, :integer, default: user&.id, null: false
-    change_column :cube_trainer_training_inputs, :user_id, :integer, default: nil
+    change_column_default :cube_trainer_training_inputs, :user_id, from: user.id, to: nil if user
     add_index :cube_trainer_training_inputs, :user_id
   end
 end
