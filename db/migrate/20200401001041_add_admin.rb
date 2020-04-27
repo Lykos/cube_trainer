7717d82ade6e68a6be281ac9cc44bf3cafa2d6c1
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-28 01:29:10 +0200

    Removed making user admin explicitly

--- db/migrate/20200401001041_add_admin.rb
+++ db/migrate/20200401001041_add_admin.rb
@@ -1,19 +1,5 @@
 class AddAdmin < ActiveRecord::Migration[6.0]
-  class User < ApplicationRecord
-  end
-
   def change
     add_column :users, :admin, :boolean, default: false
-    reversible do |dir|
-      dir.up do
-        User.reset_column_information
-        user = User.find_by(name: OsHelper.os_user)
-        user.admin = true
-        user.save!
-      end
-      dir.down do
-        # Nothing. The added data gets removed by the schema changes.
-      end
-    end
   end
 end
