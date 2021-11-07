8a58d64be50f9e6fc9b30215be0d05f70b381b10
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2021-11-07 23:45:26 +0100

    Fixed typescript typo

--- client/src/app/modes/mode-type.ts
+++ client/src/app/modes/mode-type.ts
@@ -6,7 +6,7 @@ export interface ModeType {
   readonly key: string;
   readonly name: string;
   readonly showInputModes: ShowInputMode[];
-  readonly hasBoundedInputs: bool;
+  readonly hasBoundedInputs: boolean;
   readonly cubeSizeSpec?: CubeSizeSpec;
   readonly hasGoalBadness: boolean;
   readonly buffers: string[];
