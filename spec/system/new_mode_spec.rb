7ba96fc2e358cc96048ac39c3158c30bac952abf
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2021-11-08 23:51:32 +0100

    Added system test for memo rush

--- spec/system/new_mode_spec.rb
+++ spec/system/new_mode_spec.rb
@@ -11,8 +11,7 @@ describe 'modes', type: :system do
     driven_by(:selenium_chrome_headless)
   end
 
-  it 'allows to create a new mode' do
-    visit '/login'
+  it 'allows to create a new commutator mode' do
     login(user)
 
     click_button 'Cube Trainer'
@@ -31,6 +30,32 @@ describe 'modes', type: :system do
     sleep(0.5)
     mat_select 'name', from: 'showInputMode'
     fill_in 'Goal Time per Element', with: '2.0'
+    click_button 'Next'
+
+    sleep(0.5)
+    # Not adding any stats because drag and drop is buggy with Selenium.
+    click_button 'Submit'
+
+    sleep(1)
+    expect(page).to have_text('Mode system test mode Created!')
+  end
+
+  it 'allows to create a new memo rush mode' do
+    login(user)
+
+    click_button 'Cube Trainer'
+    click_button 'New'
+
+    sleep(0.5)
+    fill_in 'Name', with: 'system test mode'
+    mat_select 'Memo Rush', from: 'modeType'
+    click_button 'Next'
+
+    sleep(0.5)
+    click_button 'Next'
+
+    sleep(0.5)
+    fill_in 'Memo Time', with: '20.0'
     click_button 'Next'
 
     sleep(0.5)
