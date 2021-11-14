592e71af2b98eca3fe1cdf46aafda168c2ccd95b
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2021-11-14 15:34:34 +0100

    Migrated inputs from letter pairs to parts

--- app/models/letter_scheme.rb
+++ app/models/letter_scheme.rb
@@ -11,11 +11,11 @@ class LetterScheme < ApplicationRecord
   alias mappings= letter_scheme_mappings=
 
   def letter(part)
-    letter_scheme_mappings.find { |e| e.part == part }&.letter
+    mappings.find { |e| e.part == part }&.letter
   end
 
   def for_letter(part_type, letter)
-    letter_scheme_mappings.find { |e| e.part_type == part_type && e.letter == letter }&.part
+    mappings.find { |e| e.part_type == part_type && e.letter == letter }&.part
   end
 
   def to_simple
---
+++ db/migrate/20211114131121_change_input_letters_to_parts.rb
@@ -0,0 +1,63 @@
+require 'twisty_puzzles'
+require 'cube_trainer/letter_pair'
+
+class ChangeInputLettersToParts < ActiveRecord::Migration[6.0]
+  class User < ApplicationRecord
+    has_many :modes
+    has_one :letter_scheme
+  end
+
+  class Mode < ApplicationRecord
+    attribute :mode_type, :mode_type
+    attribute :buffer, :part
+
+    belongs_to :user
+    has_many :inputs
+  end
+
+  class Input < ApplicationRecord
+    belongs_to :mode
+    attribute :input_representation, :input_representation
+  end
+
+  def change
+    reversible do |change|
+      change.up do
+        User.all.each do |user|
+          letter_scheme = TwistyPuzzles::BernhardLetterScheme.new
+          user.modes.each do |mode|
+            mode.inputs.each do |input|
+              next unless input.input_representation.is_a?(CubeTrainer::LetterPair)
+              next unless mode.mode_type.letter_scheme_mode
+
+              letter_pair = input.input_representation
+              part_cycle =
+                case mode.mode_type.letter_scheme_mode
+                when :buffer_plus_2_parts
+                  TwistyPuzzles::PartCycle.new([mode.buffer] + letter_pair.letters.map { |l| letter_scheme.for_letter(mode.mode_type.part_type, l) })
+                when :simple
+                  TwistyPuzzles::PartCycle.new(letter_pair.letters.map { |l| letter_scheme.for_letter(mode.mode_type.part_type, l) })
+                else
+                  raise
+                end
+              input.update(input_representation: part_cycle)
+            end
+          end
+        end
+      end
+      change.down do
+        User.all.each do |user|
+          letter_scheme = TwistyPuzzles::BernhardLetterScheme.new
+          user.modes.each do |mode|
+            mode.inputs.each do |input|
+              next unless input.input_representation.is_a?(TwistyPuzzles::PartCycle)
+              next unless mode.mode_type.letter_scheme_mode
+
+              input.update(input_representation: mode.mode_type.maybe_apply_letter_scheme(letter_scheme, input.input_representation))
+            end
+          end
+        end
+      end
+    end
+  end
+end
