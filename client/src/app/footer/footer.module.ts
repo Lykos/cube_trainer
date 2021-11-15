cb99909e3729c305ea7c2bf617c7bd64608c8a44
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2021-11-15 21:17:57 +0100

    Added privacy policy and extended footer.

--- client/src/app/footer/footer.module.ts
+++ client/src/app/footer/footer.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer.component';
+import { AboutComponent } from './about.component';
+import { ContactComponent } from './contact.component';
+import { ContactContentComponent } from './contact-content.component';
 import { PrivacyPolicyComponent } from './privacy-policy.component';
+import { CookiePolicyComponent } from './cookie-policy.component';
 import { TermsAndConditionsComponent } from './terms-and-conditions.component';
+import { ImpressumComponent } from './impressum.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -12,8 +17,13 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    ContactContentComponent,
     PrivacyPolicyComponent,
+    CookiePolicyComponent,
     TermsAndConditionsComponent,
+    ImpressumComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,8 +35,13 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    ContactContentComponent,
     PrivacyPolicyComponent,
+    CookiePolicyComponent,
     TermsAndConditionsComponent,
+    ImpressumComponent,
   ],
   providers: [],
 })
---
+++ client/src/app/footer/impressum.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cube-trainer-impressum',
+  templateUrl: './impressum.component.html',
+})
+export class ImpressumComponent {}
