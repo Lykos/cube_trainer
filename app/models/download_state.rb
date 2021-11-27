176b521cdb5413fe8f665a7486da694502615951
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2021-11-27 10:22:01 +0100

    Removed download state remainder

--- app/models/download_state.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Represents the state of downloading stuff from the global database.
-class DownloadState < ApplicationRecord
-  validates :model, uniqueness: true
-end
