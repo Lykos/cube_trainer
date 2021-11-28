aa7de666bdc2d5561336088eefc0e1366b59f707
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2021-11-28 11:40:15 +0100

    Added about section and issue reporting link

--- client/src/app/footer/contact/contact.component.ts
+++ client/src/app/footer/contact/contact.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 
-// Create and use a @cubetrainer.com address for this.
-export const CONTACT_EMAIL = 'bernhard.brodowsky+cubetrainer@gmail.com'
-
 @Component({
   selector: 'cube-trainer-contact',
   templateUrl: './contact.component.html',
