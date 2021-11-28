eb072bc047b63ca00d7a805d6d591f8062353135
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2021-11-28 10:13:49 +0100

    Fix migration

--- db/migrate/20211117170957_remove_password_digest_null.rb
+++ db/migrate/20211117170957_remove_password_digest_null.rb
@@ -1,7 +1,4 @@
 class RemovePasswordDigestNull < ActiveRecord::Migration[6.0]
-  class User < ApplicationRecord
-  end
-
   def change
     remove_column :users, :password_digest, null: false, default: ''
   end
---
+++ db/migrate/20211128090726_devise_token_auth_change_users.rb
@@ -0,0 +1,64 @@
+class DeviseTokenAuthChangeUsers < ActiveRecord::Migration[6.0]
+  class User < ApplicationRecord
+  end
+
+  def change    
+    change_table :users do |t|
+      ## Required
+      t.string :provider, null: false
+      t.string :uid, null: false
+
+      ## Database authenticatable
+      t.string :encrypted_password, null: false
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.boolean  :allow_password_change, default: false
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
+
+      ## User Info
+      # We already have our own user info
+      # t.string :name
+      # t.string :nickname
+      # t.string :image
+      # t.string :email
+
+      ## Tokens
+      t.json :tokens
+    end
+
+    reversible do |change|
+      change.up do
+        User.reset_column_information
+        User.all do |user|
+          user.update(provider: 'email', encrypted_password: '', uid: user.email)
+        end
+      end
+    end
+
+    change_column_null :users, :provider, false
+    change_column_null :users, :uid, false
+    change_column_null :users, :encrypted_password, false
+
+    # already exists
+    # add_index :users, :email,                unique: true
+    add_index :users, [:uid, :provider],     unique: true
+    add_index :users, :reset_password_token, unique: true
+    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :unlock_token,         unique: true
+  end
+end
