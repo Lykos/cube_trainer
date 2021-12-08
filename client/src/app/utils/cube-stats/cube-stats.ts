a78c87f32963831a4be6d5c0db82c4824222d242
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2021-12-08 20:30:12 +0100

    Better time reporting

--- client/src/app/utils/cube-stats/cube-stats.ts
+++ client/src/app/utils/cube-stats/cube-stats.ts
@@ -8,20 +8,37 @@ import { AlgCounts } from './alg-counts'
 import { SamplingStrategy } from './sampling-strategy';
 import { ExhaustiveSamplingStrategy } from './exhaustive-sampling-strategy';
 import { RandomSamplingStrategy } from './random-sampling-strategy';
+import { now } from '../instant';
+import { seconds } from '../duration';
 
 const numIterations = 1000;
+const slowGroupThreshold = seconds(10);
+const outputInterval = seconds(1);
 
 function expectedAlgCountsForPieces(pieces: PiecePermutationDescription, useExhaustiveSampling: boolean): AlgCounts {
   const solver = createSolver(new Decider(pieces), pieces.pieceDescription);
   const samplingStrategy: SamplingStrategy = useExhaustiveSampling ? new ExhaustiveSamplingStrategy(pieces) : new RandomSamplingStrategy(pieces, numIterations);
   const groups = samplingStrategy.groups();
-  let groupsDone = -1;
-  const start = Date.now()
+  let groupsDone = 0;
+  const start = now()
+  let lastOutput = start;
   return expectedValue(groups.flatMap(group => {
+    const groupStart = now();
+    const algCounts = solver.algCounts(group);
+    const groupEnd = now();
     ++groupsDone;
-    const elapsedS = (Date.now() - start) / 1000;
-    console.log(`${groupsDone / groups.length} done (${groupsDone} / ${groups.length}) with a rate of ${groupsDone / elapsedS} groups per second`);
-    return solver.algCounts(group);
+    const elapsedSinceLastOutput = groupEnd.minusInstant(lastOutput);
+    const groupElapsed = groupEnd.minusInstant(groupStart);
+    const totalElapsed = groupEnd.minusInstant(start);
+    if (elapsedSinceLastOutput.greaterThan(outputInterval)) {
+      lastOutput = groupEnd;
+      const groupsPerSecond = groupsDone / totalElapsed.toSeconds();
+      console.log(`${groupsDone / groups.length} done (${groupsDone} / ${groups.length}) with a rate of ${groupsPerSecond} groups per second`);
+    }
+    if (groupElapsed.greaterThan(slowGroupThreshold)) {
+      console.log(`Slow group (${groupElapsed.toSeconds()} seconds)`, group);
+    }
+    return algCounts;
   }));
 }
 
