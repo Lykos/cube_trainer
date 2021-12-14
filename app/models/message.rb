8aa91decd2d310bee3a31e2e484d0bc4be710ca7
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2021-12-14 03:05:03 +0100

    Rubocop fixes

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,16 +5,14 @@ class Message < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   after_create :broadcast_message, :broadcast_unread_messages_count
-  after_update :broadcast_unread_messages_count, if: ->{ saved_change_to_read? }
+  after_update :broadcast_unread_messages_count, if: -> { saved_change_to_read? }
   after_destroy :broadcast_unread_messages_count
 
   def to_dump
     attributes
   end
 
-  def broadcast_unread_messages_count
-    user.broadcast_unread_messages_count
-  end
+  delegate :broadcast_unread_messages_count, to: :user
 
   def broadcast_message
     MessageChannel.broadcast_to(user, title: title)
