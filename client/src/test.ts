d0acd3b6cbde8984018eb48000f06870d0ae1fa9
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2022-01-08 14:57:16 +0100

    Added stopwatch tests

--- client/src/test.ts
+++ client/src/test.ts
@@ -1,6 +1,10 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+// This is needed to make fake Jasmine clock work.
+(window as any)['__zone_symbol__fakeAsyncPatchLock'] = true;
+
 import 'zone.js/testing';
+
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
