6daa745b69a7791596e02ba0ffe7f578403ee0e4
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2022-01-13 14:01:47 +0100

    Removed sleep from new_color_scheme_spec

--- spec/system/new_color_scheme_spec.rb
+++ spec/system/new_color_scheme_spec.rb
@@ -16,14 +16,11 @@ describe 'new color scheme', type: :system do
     click_link user.name
     click_link 'Create Color Scheme'
 
-    sleep(0.5)
     mat_select 'Yellow', id: 'colorSelectU'
     mat_select 'Red', id: 'colorSelectF'
 
-    sleep(0.5)
     click_button 'Submit'
 
-    sleep(1)
     expect(page).to have_text('Color scheme created!')
   end
 end
