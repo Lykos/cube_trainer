7b66115afece65f3904d1a5df4d24dd83a48cfbf
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2022-01-22 16:22:48 +0100

    sleep for mat stepper

--- spec/system/new_training_session_spec.rb
+++ spec/system/new_training_session_spec.rb
@@ -2,6 +2,19 @@
 
 require 'rails_helper'
 
+# We sleep before clicking 'Next' in the mat-stepper.
+# Sleep is ugly and we hate it, but we have done everything we could think of
+# and at this point we don't understand why it's flaky without sleep.
+# What we did to make sure that in theory we shouldn't need sleep:
+# * Always use Capybara matchers that implicitly wait.
+# * Make sure the 'Next' button is disabled in the frontend before it's ready to be clicked.
+# * Introduce some fake HTML elements containing the entered data and
+#   check if the entered form data has arrived there. This should avoid problem where the data
+#   hasn't propagated yet.
+def before_next_sleep
+  sleep(0.1)
+end
+
 describe 'new training session', type: :system do
   include_context 'with user abc'
   include_context 'with alg set'
@@ -21,24 +34,29 @@ describe 'new training session', type: :system do
     fill_in 'Name', with: 'system test training session'
     mat_select 'Edge Commutators', from: 'trainingSessionType'
     expect(find('#training-session-type-id', visible: :all)).to have_text(:all, 'edge_commutators')
+    before_next_sleep
     within('#training-session-type-group-actions') { click_button 'Next' }
 
     mat_select 'UF', from: 'buffer'
     expect(find('#buffer', visible: :all)).to have_text(:all, 'UF')
+    before_next_sleep
     within('#setup-group-actions') { click_button 'Next' }
 
     mat_select 'Testy Testikow', id: 'alg-set-select'
     find(:css, '#exclude-alg-holes').set(true)
     expect(find('#exclude-alg-holes-value', visible: :all)).to have_text(:all, 'true')
+    before_next_sleep
     within('#alg-set-group-actions') { click_button 'Next' }
 
     mat_select 'picture', from: 'showInputMode'
     fill_in 'Goal Time per Element', with: '2.0'
     expect(find('#show-input-mode', visible: :all)).to have_text(:all, 'picture')
     expect(find('#goal-badness', visible: :all)).to have_text(:all, '2')
+    before_next_sleep
     within('#training-group-actions') { click_button 'Next' }
 
     # Not adding any stats because drag and drop is buggy with Selenium.
+    before_next_sleep
     within('#stats-group-actions') { click_button 'Submit' }
 
     expect(page).to have_text('Session system test training session created.')
@@ -54,6 +72,7 @@ describe 'new training session', type: :system do
     fill_in 'Name', with: 'system test training session'
     mat_select 'Corner Commutators', from: 'trainingSessionType'
     expect(find('#training-session-type-id', visible: :all)).to have_text(:all, 'corner_commutators')
+    before_next_sleep
     within('#training-session-type-group-actions') { click_button 'Next' }
 
     within('#cube-size-input') do
@@ -62,15 +81,18 @@ describe 'new training session', type: :system do
     expect(find('#cube-size', visible: :all)).to have_text(:all, '3')
     mat_select 'ULB', from: 'buffer'
     expect(find('#buffer', visible: :all)).to have_text(:all, 'ULB')
+    before_next_sleep
     within('#setup-group-actions') { click_button 'Next' }
 
     mat_select 'name', from: 'showInputMode'
     fill_in 'Goal Time per Element', with: '2.0'
     expect(find('#show-input-mode', visible: :all)).to have_text(:all, 'name')
     expect(find('#goal-badness', visible: :all)).to have_text(:all, '2')
+    before_next_sleep
     within('#training-group-actions') { click_button 'Next' }
 
     # Not adding any stats because drag and drop is buggy with Selenium.
+    before_next_sleep
     within('#stats-group-actions') { click_button 'Submit' }
 
     expect(page).to have_text('Session system test training session created.')
@@ -86,19 +108,23 @@ describe 'new training session', type: :system do
     fill_in 'Name', with: 'system test training session'
     mat_select 'Memo Rush', from: 'trainingSessionType'
     expect(find('#training-session-type-id', visible: :all)).to have_text(:all, 'memo_rush')
+    before_next_sleep
     within('#training-session-type-group-actions') { click_button 'Next' }
 
     within('#cube-size-input') do
       fill_in 'Cube Size', with: '3'
     end
     expect(find('#cube-size', visible: :all)).to have_text(:all, '3')
+    before_next_sleep
     within('#setup-group-actions') { click_button 'Next' }
 
     fill_in 'Memo Time', with: '20.0'
     expect(find('#memo-time-s', visible: :all)).to have_text(:all, '20')
+    before_next_sleep
     within('#training-group-actions') { click_button 'Next' }
 
     # Not adding any stats because drag and drop is buggy with Selenium.
+    before_next_sleep
     within('#stats-group-actions') { click_button 'Submit' }
 
     expect(page).to have_text('Session system test training session created.')
