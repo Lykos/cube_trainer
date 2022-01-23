d058a418865bbbba2a9671fec0ec4dbf712ccd9e
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2022-01-23 16:24:42 +0100

    Removed tests for email existence

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,26 +10,6 @@ RSpec.describe 'Users', type: :request do
   include_context 'with user auth headers'
   include_context 'with eve auth headers'
 
-  describe 'GET #name_or_email_exists?' do
-    it 'returns true if a user exists' do
-      get '/api/name_or_email_exists', params: { name_or_email: user.name }, headers: user_headers
-      expect(response).to have_http_status(:success)
-      expect(JSON.parse(response.body)).to eq(true)
-    end
-
-    it 'returns true if an email exists' do
-      get '/api/name_or_email_exists', params: { name_or_email: user.email }, headers: user_headers
-      expect(response).to have_http_status(:success)
-      expect(JSON.parse(response.body)).to eq(true)
-    end
-
-    it "returns false if an name/email doesn't exist" do
-      get '/api/name_or_email_exists', params: { name_or_email: 'grmlefex' }, headers: user_headers
-      expect(response).to have_http_status(:success)
-      expect(JSON.parse(response.body)).to eq(false)
-    end
-  end
-
   describe 'GET #show' do
     it 'returns http success' do
       get '/api/user', headers: user_headers
