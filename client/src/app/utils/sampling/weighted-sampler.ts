80b2529c6e2306866ae6ae041afcb7f8267db5f4
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2022-05-26 13:34:23 +0200

    Fixed inconsistency between samper.ready and sampler.sample

--- client/src/app/utils/sampling/weighted-sampler.ts
+++ client/src/app/utils/sampling/weighted-sampler.ts
@@ -5,6 +5,7 @@ import { SamplingState } from './sampling-state';
 import { WeightState } from './weight-state';
 import { SamplingError } from './sampling-error';
 import { weightedDraw } from './weighted-draw';
+import { Optional, some, none, orElse } from '@utils/optional';
 
 function isRecent<X>(samplingState: SamplingState<X>, weightState: WeightState, recencyThreshold: number) {
   // In case of very few total items, we have to soften the recency threshold.
@@ -12,9 +13,16 @@ function isRecent<X>(samplingState: SamplingState<X>, weightState: WeightState, 
   return weightState.itemsSinceLastOccurrence < adjustedRecencyThreshold;
 }
 
+interface WeightedItem<X> {
+  readonly item: X;
+  readonly weight: number;
+}
+
 // A simple sampler (i.e. it is not a combination of other samplers) that ignores
 // recent items and uses the weight from the given weighter to draw an item.
 export class WeightedSampler implements Sampler {
+  cachedWeightedItems: Optional<WeightedItem<any>[]> = none;
+
   constructor(private readonly name: string,
               private readonly weighter: Weighter,
               private readonly recencyThreshold: number) {}  
@@ -29,13 +37,19 @@ export class WeightedSampler implements Sampler {
       throw new SamplingError(`no item has positive weight in sampler ${this.name}`);
     }
     const item = weightedDraw(weightedItems).item;
+    this.cachedWeightedItems = none;
     return { item, samplerName: this.name };
   }
 
-  private weightedItems<X>(state: SamplingState<X>) {
-    return state.weightStates
-      .filter(s => !isRecent(state, s.state, this.recencyThreshold))
-      .map(s => ({ item: s.item, weight: this.weighter.weight(s.state) }))
-      .filter(s => s.weight > 0);
+  private weightedItems<X>(state: SamplingState<X>): WeightedItem<X>[] {
+    const result = orElse(
+      this.cachedWeightedItems,
+      state.weightStates
+	.filter(s => !isRecent(state, s.state, this.recencyThreshold))
+	.map(s => ({ item: s.item, weight: this.weighter.weight(s.state) }))
+	.filter(s => s.weight > 0)
+    );
+    this.cachedWeightedItems = some(result);
+    return result;
   }
 }
