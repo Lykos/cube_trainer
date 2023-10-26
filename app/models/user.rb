5ce1b830ed80b6c75ffad9f976537f1afc1aed16
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2023-10-26 23:23:11 +0200

    Rescue redis connection errors.

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,8 @@ class User < ApplicationRecord
 
   def broadcast_unread_messages_count
     UnreadMessagesCountChannel.broadcast_to(self, unread_messages_count: unread_messages_count)
+  rescue Redis::CannotConnectError => e
+    Rails.logger.error 'Broadcasting message failed: #{e}'
   end
 
   private
