4a8aad0f3d7982b24dae15f0b02fc78f70d4a3ca
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2023-10-31 22:36:22 +0100

    Improved test

--- lib/cube_trainer/sheet_scraping/case_set_setup_finder.rb
+++ lib/cube_trainer/sheet_scraping/case_set_setup_finder.rb
@@ -4,62 +4,64 @@ require 'twisty_puzzles'
 require 'cube_trainer/sheet_scraping/case_set_seed_algs'
 
 module CubeTrainer
-  # Finds setups for each case in a case set.
-  # Note that the setups might be bad algs for humans, but they might be useful for machine processing.
-  class CaseSetSetupFinder
-    def initialize(case_set)
-      raise TypeError unless case_set.is_a?(CaseSets::ConcreteCaseSet)
+  module SheetScraping
+    # Finds setups for each case in a case set.
+    # Note that the setups might be bad algs for humans, but they might be useful for machine processing.
+    class CaseSetSetupFinder
+      def initialize(case_set)
+        raise TypeError unless case_set.is_a?(CaseSets::ConcreteCaseSet)
 
-      @case_set = case_set
-    end
-
-    attr_reader :case_set
-
-    def find_setup(casee)
-      raise ArgumentError unless casee.valid?
-      raise ArgumentError unless @case_set.match?(casee)
-
-      setup_hash[casee.canonicalize(ignore_same_face_center_cycles: true)]
-    end
-
-    private
-
-    include TwistyPuzzles
-    include CaseSetSeedAlgs
-
-    def case_reverse_engineer
-      @case_reverse_engineer ||=
-        CubeTrainer::CaseReverseEngineer.new(
-          cube_size: @case_set.default_cube_size
-        )
-    end
-
-    def create_setup_hash
-      setup_hash = {}
-      cases = @case_set.cases
-      algs = initial_algs
-
-      5.times do
-        useful_algs = []
-        algs.each do |alg|
-          casee = case_reverse_engineer.find_case(alg)
-
-          found_cases = cases.select { |c| c.equivalent?(casee) }
-          next if found_cases.empty?
-
-          useful_algs.push(alg)
-          found_cases.each { |c| setup_hash[c] = alg }
-          cases -= found_cases
-        end
-        return setup_hash if cases.empty?
-
-        algs = useful_algs.flat_map { |alg| setup_algs.flat_map { |s| s + alg + s.inverse } }
+        @case_set = case_set
       end
-      raise "Couldn't find algs for all cases for #{@case_set.class}." unless cases.empty?
-    end
 
-    def setup_hash
-      @setup_hash ||= create_setup_hash
+      attr_reader :case_set
+
+      def find_setup(casee)
+        raise ArgumentError unless casee.valid?
+        raise ArgumentError unless @case_set.match?(casee)
+
+        setup_hash[casee.canonicalize(ignore_same_face_center_cycles: true)]
+      end
+
+      private
+
+      include TwistyPuzzles
+      include CaseSetSeedAlgs
+
+      def case_reverse_engineer
+        @case_reverse_engineer ||=
+          CubeTrainer::CaseReverseEngineer.new(
+            cube_size: @case_set.default_cube_size
+          )
+      end
+
+      def create_setup_hash
+        setup_hash = {}
+        cases = @case_set.cases
+        algs = initial_algs
+
+        5.times do
+          useful_algs = []
+          algs.each do |alg|
+            casee = case_reverse_engineer.find_case(alg)
+
+            found_cases = cases.select { |c| c.equivalent?(casee) }
+            next if found_cases.empty?
+
+            useful_algs.push(alg)
+            found_cases.each { |c| setup_hash[c] = alg.inverse }
+            cases -= found_cases
+          end
+          return setup_hash if cases.empty?
+
+          algs = useful_algs.flat_map { |alg| setup_algs.flat_map { |s| s + alg + s.inverse } }
+        end
+        raise "Couldn't find algs for all cases for #{@case_set.class}." unless cases.empty?
+      end
+
+      def setup_hash
+        @setup_hash ||= create_setup_hash
+      end
     end
   end
 end
--- spec/cube_trainer/sheet_scraping/case_set_setup_finder_spec.rb
+++ spec/cube_trainer/sheet_scraping/case_set_setup_finder_spec.rb
@@ -6,16 +6,32 @@ require 'cube_trainer/sheet_scraping/case_set_setup_finder'
 require 'twisty_puzzles'
 require 'rails_helper'
 
-describe CubeTrainer::CaseSetSetupFinder do
+def canonicalize(casee)
+  casee.canonicalize(ignore_same_face_center_cycles: true)
+end
+
+describe CubeTrainer::SheetScraping::CaseSetSetupFinder do
   with_buffer = CaseSets::AbstractCaseSet.all.select(&:buffer?)
   without_buffer = CaseSets::AbstractCaseSet.all.reject(&:buffer?)
   without_buffer_refinements = without_buffer.map(&:refinement)
   with_buffer_refinements = with_buffer.map { |c| c.refinement(c.buffer_part_type::ELEMENTS.first) }
   concrete_case_sets = with_buffer_refinements + without_buffer_refinements
 
-  concrete_case_sets.each do |concrete_case_set|
-    it "Finds a setup for case #{concrete_case_set.cases.first} in #{concrete_case_set}." do
-      CaseSetSetupFinder.new(concrete_case_set).find_setup(concrete_case_set.cases.first)
+  concrete_case_sets.each do |case_set|
+    context case_set do
+      let(:case_reverse_engineer) do
+        CubeTrainer::CaseReverseEngineer.new(
+          cube_size: case_set.default_cube_size
+        )
+      end
+
+      casee = case_set.cases.first
+
+      fit "Finds a setup for case #{casee} in #{case_set}." do
+        alg = SheetScraping::CaseSetSetupFinder.new(case_set).find_setup(casee)
+        found_case = case_reverse_engineer.find_case(alg.inverse)
+        expect(canonicalize(found_case)).to eq(canonicalize(casee))
+      end
     end
   end
 end
