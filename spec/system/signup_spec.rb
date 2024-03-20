45317639c19488d70c7acc4f0788aa2eefba852b
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2024-03-20 23:16:38 +0100

    Made signup test actually use the email

--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -2,7 +2,6 @@
 
 require 'rails_helper'
 
-# TODO: Doesn't work on github for some reason.
 describe 'signup' do
   before do
     driven_by(:selenium_chrome_headless)
@@ -12,7 +11,7 @@ describe 'signup' do
     visit ''
     click_link_or_button 'Sign Up'
 
-    fill_in 'Username', with: 'system test user'
+    fill_in 'Username', with: 'signup test user'
     fill_in 'Email', with: 'system_test+signup@example.org'
     find_by_id('password').fill_in 'Password', with: 'password'
     fill_in 'Confirm Password', with: 'password'
@@ -27,17 +26,15 @@ describe 'signup' do
     confirm_path = extract_first_link_path(confirmation_email)
     expect(confirm_path).to start_with('/api/auth/confirmation')
 
-    # TODO: Fix this
-    # visit confirm_path
-    # expect(page).to have_text('Email Confirmed')
-    # TODO: Remove this
+    visit replaced_redirect_url(confirm_path)
+    expect(page).to have_text('Email Confirmed')
     user = User.find_by!(email: 'system_test+signup@example.org')
-    user.confirm
+    expect(user).to be_confirmed
 
     # Note that the password can't be read from the database, so we create the user manually.
-    user = User.new(name: 'system test user', email: 'system_test+signup@example.org', password: 'password')
+    user = User.new(name: 'signup test user', email: 'system_test+signup@example.org', password: 'password')
     login(user)
-    expect(page).to have_text('system test user')
+    expect(page).to have_text('signup test user')
     expect(page).to have_text('Logout')
   end
 end
