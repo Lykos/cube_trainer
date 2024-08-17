ff36335846d69a12f6ca856c259180057fe60880
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2024-08-17 21:28:14 +0200

    Updated rails config defaults

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module CubeTrainer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.1
 
     # TODO enable
     # config.api_only = true
