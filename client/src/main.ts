a511ec6a42d50073b8c7ebe9357f39dab27140bd
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2024-12-21 17:48:10 +0100

    Unmigrated main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,12 @@
 import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppComponent } from './app/app.component';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
