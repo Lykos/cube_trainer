03c95385d29620d6a13c727b9c4c6877f963288e
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2024-12-21 17:42:37 +0100

    Tried to migrate to standalone app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { Component } from '@angular/core';
 import { ToolbarComponent } from '@core/toolbar/toolbar.component';
 import { FooterComponent } from '@core/footer/footer.component';
+import { SharedModule } from '@shared/shared.module';
 
 @Component({
   selector: 'cube-trainer',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [ToolbarComponent, FooterComponent, AppRoutingModule],
+  imports: [ToolbarComponent, FooterComponent, AppRoutingModule, SharedModule],
 })
 export class AppComponent {
 }
